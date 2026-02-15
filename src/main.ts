2fc604558cc14e060d25914a0b0bd246ade48639
Pedro <pedrouzcategui0103@gmail.com>
2026-02-15 15:33:24 -0400

    Add Products Routes to App

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express, { type Request, type Response } from "express";
+import productRoutes from "./routers/product.routes.ts";
 
 const app = express();
 const port = 3000;
@@ -6,6 +7,8 @@ const port = 3000;
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
+
+app.use("/products", productRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
